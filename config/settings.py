d956210fe1081aba9062fd59fa4ed6de26d3dd51
jjuarez26base <jjuarez26@basecampcodingacademy.org>
2026-01-12 16:15:28 -0600

    Trying to fix static files bug

--- config/settings.py
+++ config/settings.py
@@ -128,5 +128,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'app/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
